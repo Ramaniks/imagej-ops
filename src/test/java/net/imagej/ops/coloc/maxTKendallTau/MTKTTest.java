9026e7121d191d0804d167b58b5fd3cc8802d205
Ellen T Arena <arenaellen@gmail.com>
2018-06-01 15:07:11 -0500

    MTKTTest: WIP ...
    And the WIP continues ...

--- src/test/java/net/imagej/ops/coloc/maxTKendallTau/MTKTTest.java
+++ src/test/java/net/imagej/ops/coloc/maxTKendallTau/MTKTTest.java
@@ -32,11 +32,21 @@ package net.imagej.ops.coloc.maxTKendallTau;
 
 import static org.junit.Assert.assertEquals;
 
-import net.imagej.ops.AbstractOpTest;
+import net.imagej.ops.Ops;
+import net.imagej.ops.Ops.Coloc.MaxTKendallTau;
+import net.imagej.ops.coloc.ColocalisationTest;
+import net.imagej.ops.coloc.pValue.PValueResult;
+import net.imagej.ops.coloc.pearsons.DefaultPearsons;
+import net.imagej.ops.special.function.AbstractBinaryFunctionOp;
+import net.imagej.ops.special.function.BinaryFunctionOp;
+import net.imagej.ops.special.function.Functions;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
 
 import org.junit.Test;
 
@@ -47,7 +57,7 @@ import org.junit.Test;
  * @author Shulei Wang
  * @param <V>
  */
-public class MTKTTest<V extends RealType<V>> extends AbstractOpTest {
+public class MTKTTest<T extends RealType<T>, U extends RealType<U>> extends ColocalisationTest {
 	
 	// Ranking data, test the rankTransformation() function. There are
 	// two cases: 1) no tie breaking, test if this function
@@ -130,7 +140,7 @@ public class MTKTTest<V extends RealType<V>> extends AbstractOpTest {
 		Img<DoubleType> vImage1 = ArrayImgs.doubles(values1, values1.length);
 		Img<DoubleType> vImage2 = ArrayImgs.doubles(values2, values2.length);
 		double expectedMTKT = -1;
-		double mtkt = MTKT.calculateKendallTau(values, final List<Integer> activeIndex)
+		//double mtkt = MTKT.calculateKendallTau(values, final List<Integer> activeIndex);
 		//System.out.println(mtkt);
 		
 	}
@@ -140,15 +150,47 @@ public class MTKTTest<V extends RealType<V>> extends AbstractOpTest {
 	// image and compare the result with results from R function. Third, we can
 	// test the real image and compare the result with results from R function.
 	@Test
-	public void testMTKT() {
-		
-		
+	public void testMTKTZeroCorr() {
+		double result = (Double) ops.run(MTKT.class, zeroCorrelationImageCh1, zeroCorrelationImageCh2);
+		assertEquals(2.5, result, 0.5);
+		//System.out.println("zeroCorr mtkt value = " + result);
+	}
+	@Test
+	public void testMTKTPosCorr() {
+		double result = (Double) ops.run(MTKT.class, positiveCorrelationImageCh1, positiveCorrelationImageCh2);
+		assertEquals(58, result, 1);
+		//System.out.println("posCorr mtkt value = " + result2);
 	}
 
 	// Checks calculated pValue for MTKT.
 	@Test
 	public void testMTKTpValue() {
-		
-		
+		final double mean = 0.2;
+		final double spread = 0.1;
+		final double[] sigma = new double[] { 3.0, 3.0 };
+		Img<FloatType> ch1 = ColocalisationTest.produceMeanBasedNoiseImage(new FloatType(), 24, 24,
+			mean, spread, sigma, 0x01234567);
+		Img<FloatType> ch2 = ColocalisationTest.produceMeanBasedNoiseImage(new FloatType(), 24, 24,
+			mean, spread, sigma, 0x98765432);
+		BinaryFunctionOp<RandomAccessibleInterval<FloatType>, RandomAccessibleInterval<FloatType>, Double> op =
+			Functions.binary(ops, MaxTKendallTau.class, Double.class, ch1, ch2);
+		PValueResult value = (PValueResult) ops.run(Ops.Coloc.PValue.class, new PValueResult(), ch1, ch2, op);
+		assertEquals(0.274, value.getPValue(), 0.0);
+	}
+	@Test
+	public void testMTKTpValueZeroCorr() {
+		BinaryFunctionOp<Img<UnsignedByteType>, Img<UnsignedByteType>, Double> op =
+				Functions.binary(ops, MaxTKendallTau.class, Double.class, zeroCorrelationImageCh1, zeroCorrelationImageCh2);
+			PValueResult value = (PValueResult) ops.run(Ops.Coloc.PValue.class, new PValueResult(), zeroCorrelationImageCh1, zeroCorrelationImageCh2, op);
+			System.out.println("zeroCorr p value = " + value.getPValue());
+			//assertEquals(0.274, value.getPValue(), 0.0);
+	}
+	@Test
+	public void testMTKTpValuePosCorr() {
+		BinaryFunctionOp<Img<UnsignedByteType>, Img<UnsignedByteType>, Double> op =
+				Functions.binary(ops, MaxTKendallTau.class, Double.class, positiveCorrelationImageCh1, positiveCorrelationImageCh2);
+			PValueResult value = (PValueResult) ops.run(Ops.Coloc.PValue.class, new PValueResult(), positiveCorrelationImageCh1, positiveCorrelationImageCh2, op);
+			System.out.println("posCorr p value = " + value.getPValue());
+			//assertEquals(0.274, value.getPValue(), 0.0);
 	}
 }
